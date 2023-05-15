071f2135a3be84e9d17e020714a92651bdd0f780
teraclitos <tefans12@gmail.com>
2023-05-15 18:38:13 -0300

    agrego font-family del titulo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "../styles/all.css";
 
 const NavBar = () => {
-  return <div>Francisco Terán</div>;
+  return (
+    <div className="py-3">
+      <h1 className="portfolio-name">Francisco Terán</h1>
+    </div>
+  );
 };
 
 export default NavBar;
