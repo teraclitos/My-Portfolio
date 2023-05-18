cda58d01b0434bb35a06245fc03ec9ea6f2608f9
teraclitos <tefans12@gmail.com>
2023-05-18 20:33:37 -0300

    agrego funcionalidad al libro

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -4,14 +4,13 @@ import "../styles/all.css";
 
 const BookContainer = () => {
   const dataBook = [
-    { title1: "", title2: "Title1", text1: "", text2: "hola" },
+    { title1: "", title2: "", text1: "", text2: "" },
+    { title1: "Índice", title2: "Title1", text1: "hola", text2: "hola" },
     { title1: "Title2", title2: "Title3", text1: "hola", text2: "hola" },
     { title1: "Title4", title2: "Title5", text1: "hola", text2: "hola" },
     { title1: "Title6", title2: "Title7", text1: "hola", text2: "hola" },
     { title1: "Title8", title2: "Title9", text1: "hola", text2: "hola" },
-    { title1: "Title10", title2: "Title11", text1: "hola", text2: "hola" },
-
-    { title1: "Title12", title2: "", text1: "hola", text2: "" },
+    { title1: "Title10", title2: "", text1: "hola", text2: "" },
   ];
 
   const indexInitial = dataBook.map((element, i) => i).reverse();
@@ -23,15 +22,59 @@ const BookContainer = () => {
   const [translate, setTranslate] = useState(translateInitial);
   const functionTranslateFrontPage = (d) => {
     const newTranslate = translate.map((element, i) =>
-      i === d ? (element = -180) : element
+      i === d ? -180 : element
     );
 
     setTranslate(newTranslate);
   };
-  const functionTranslateBackPage = (d) => {
-    const newTranslate = translate.map((element, i) =>
-      i === d ? (element = 0) : element
+
+  const functionPageIndex = (proyectoId) => {
+    const numberOfTransaltions = Math.ceil(proyectoId / 2);
+    const arrayTranslation = Array.from(
+      { length: numberOfTransaltions },
+      (_, index) => index + 1
     );
+
+    arrayTranslation.forEach((element, i) => {
+      setPointerEvent("none");
+      setTimeout(() => {
+        let newTranslate = translate.map((element, index) =>
+          index <= i + 1 ? -180 : element
+        );
+
+        const newindex = indexes.map((element, i3) => {
+          if (i + 1 === i3) {
+            return dataBook.length - 1;
+          } else {
+            if (i + 1 === i3 - 1) {
+              return dataBook.length - 2;
+            } else if (i + 1 === dataBook.length - 1) {
+              return dataBook.length - 2;
+            } else {
+              return dataBook.length - 3;
+            }
+          }
+        });
+
+        setIndexes(newindex);
+
+        console.log(newTranslate);
+
+        setTimeout(() => {
+          setTranslate(newTranslate);
+        }, 200);
+
+        if (i === arrayTranslation.length - 1) {
+          setTimeout(() => {
+            setPointerEvent("all");
+          }, 700);
+        }
+      }, (i + 1) * 400);
+    });
+  };
+
+  const functionTranslateBackPage = (d) => {
+    const newTranslate = translate.map((element, i) => (i === d ? 0 : element));
 
     setTranslate(newTranslate);
   };
@@ -53,10 +96,6 @@ const BookContainer = () => {
 
     setIndexes(newindex);
   };
-
-  useEffect(() => {
-    console.log(indexes);
-  }, [indexes]);
 
   return (
     <div
@@ -89,7 +128,16 @@ const BookContainer = () => {
             className={i == 0 ? "face-front portada" : "face-front"}
             style={{ pointerEvents: pointerEvent }}
           >
-            <h1>{element.title1}</h1>
+            <h1
+              onClick={(e) => {
+                e.stopPropagation();
+                {
+                  i === 1 && functionPageIndex(5);
+                }
+              }}
+            >
+              {element.title1}
+            </h1>
             <p>{element.text1}</p>
           </div>
           <div
