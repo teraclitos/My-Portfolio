44ee914532d008f0042dca3d451f5d0e9d33e141
teraclitos <tefans12@gmail.com>
2023-05-27 20:12:43 -0300

    agrego efecto cruz al navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "../styles/all.css";
-import { faLanguage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = ({
   language,
@@ -52,54 +54,56 @@ const NavBar = ({
     language === "spanish" ? setLanguage("english") : setLanguage("spanish");
   };
   return (
-    <div
-      style={{ height: heightNav }}
-      className="py-5 pe-lg-5 ps-lg-5  d-block d-lg-flex justify-content-between align-items-center nav-bar-container "
-    >
-      <h1 className="portfolio-name text-center ">
-        {widthScreen > 992 ? "Francisco Terán" : "F T"}
-      </h1>
-      <div className=" d-block d-lg-flex justify-content-around ms-lg-5  align-items-center">
-        {navBarLinks.map((element) => (
-          <div className="link-nav text-center ">
-            <span
-              onClick={() => {
-                if (element === "Proyectos" || element === "Proyects") {
-                  proyectFunctionBook();
-                }
-              }}
-            >
-              {element}
-            </span>
-          </div>
-        ))}
+    <nav className="nav-bar-container">
+      <div
+        style={{ height: heightNav }}
+        className="py-5 pe-lg-5 ps-lg-5  d-block d-lg-flex justify-content-between align-items-center nav-barr "
+      >
+        <h1 className="portfolio-name text-center ">
+          {widthScreen > 992 ? "Francisco Terán" : "F T"}
+        </h1>
+        <div className=" d-block d-lg-flex justify-content-around ms-lg-5  align-items-center">
+          {navBarLinks.map((element) => (
+            <div className="link-nav text-center ">
+              <span
+                onClick={() => {
+                  if (element === "Proyectos" || element === "Proyects") {
+                    proyectFunctionBook();
+                  }
+                }}
+              >
+                {element}
+              </span>
+            </div>
+          ))}
 
+          <div
+            className="py-3 d-none d-lg-flex ms-lg-5 justify-content-center wooden-button "
+            onClick={() => {
+              functionLanguage();
+            }}
+          >
+            {language}
+          </div>
+        </div>
         <div
-          className="py-3 d-none d-lg-flex ms-lg-5 justify-content-center wooden-button "
+          className="py-3 d-flex d-lg-none justify-content-center wooden-button wooden-button-responsive "
           onClick={() => {
             functionLanguage();
           }}
         >
           {language}
         </div>
+
+        <FontAwesomeIcon
+          onClick={() => {
+            heightNav === "5em" ? setHeightNav("20em") : setHeightNav("5em");
+          }}
+          className="responsive-open d-flex d-lg-none"
+          icon={heightNav === "5em" ? faBars : faXmark}
+        />
       </div>
-      <div
-        className="py-3 d-flex d-lg-none justify-content-center wooden-button wooden-button-responsive "
-        onClick={() => {
-          functionLanguage();
-        }}
-      >
-        {language}
-      </div>
-      <div
-        onClick={() => {
-          heightNav === "5em" ? setHeightNav("20em") : setHeightNav("5em");
-        }}
-        className="py-3 d-flex d-lg-none justify-content-center wooden-button wooden-button-responsive-open "
-      >
-        open
-      </div>
-    </div>
+    </nav>
   );
 };
 
