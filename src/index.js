3b9af52574edeba76d2c892bd6d091d9e03ba739
Francisco <tefans12@gmail.com>
2024-11-19 12:37:57 -0300

    elimino archivos innecesarios

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-import store from './store/store.js'
-import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+
+    <App />
+
   </React.StrictMode>
 )
